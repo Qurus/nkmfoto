3567f22ff48b1722a27217d386c1861a783b711b
ali uen <flashnukers@gmail.com>
2019-03-13 13:07:46 +0100

    foto.js cleaned

--- pages/foto.js
+++ pages/foto.js
@@ -18,69 +18,69 @@ export default () => (
 
                             <div class="mfp-gallery pw-collage pw-collage-loading load-one-by-one" data-row-height="360" data-mobile-row-height="120" data-effect="effect-4">
                            
-                                <a href="../static/images/home/01.jpg" title="Portrait of a young man, studio shooting.">
-                                  <img src="../static/images/home/01.jpg" width="500" height="500" alt="image" />
+                                <a href={require('../static/images/home/01.jpg')} title="Portrait of a young man, studio shooting.">
+                                  <img src={require('../static/images/home/01.jpg')} width="500" height="500" alt="image" />
                                 </a>
                                 
-                                <a href="../static/images/home/02.jpg">
-                                  <img src="../static/images/home/02.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/02.jpg')}>
+                                  <img src={require('../static/images/home/02.jpg')} width="500" height="333" alt="image" />
                                 </a>
                                 
-                                <a href="../static/images/home/03.jpg">
-                                  <img src="../static/images/home/03.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/03.jpg')}>
+                                  <img src={require('../static/images/home/03.jpg')} width="500" height="333" alt="image" />
                                 </a>
                                 
-                                <a href="../static/images/home/04.jpg">
-                                <img src="../static/images/home/04.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/04.jpg')}>
+                                <img src={require('../static/images/home/04.jpg')} width="500" height="333" alt="image" />
                                 </a>
                                 
                                 
-                                <a href="../static/images/home/05.jpg">
-                                  <img src="../static/images/home/05.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/05.jpg')}>
+                                <img src={require('../static/images/home/05.jpg')} width="500" height="333" alt="image" />
                                 </a>
                                 
-                                <a href="../static/images/home/06.jpg">
-                                  <img src="../static/images/home/06.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/06.jpg')}>
+                                <img src={require('../static/images/home/06.jpg')} width="500" height="333" alt="image" />
                                 </a>
 
-                                <a href="../static/images/home/07.jpg">
-                                  <img src="../static/images/home/07.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/07.jpg')}>
+                                <img src={require('../static/images/home/07.jpg')} width="500" height="333" alt="image" />
                                 </a>
 
-                                <a href="../static/images/home/08.jpg">
-                                  <img src="../static/images/home/08.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/08.jpg')}>
+                                <img src={require('../static/images/home/08.jpg')} width="500" height="333" alt="image" />
                                 </a>
 
-                                <a href="../static/images/home/09.jpg">
-                                  <img src="../static/images/home/09.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/09.jpg')}>
+                                <img src={require('../static/images/home/09.jpg')} width="500" height="333" alt="image" />
                                 </a>
 
-                                <a href="../static/images/home/10.jpg">
-                                  <img src="../static/images/home/10.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/10.jpg')}>
+                                <img src={require('../static/images/home/10.jpg')} width="500" height="333" alt="image" />
                                 </a>
 
-                                <a href="../static/images/home/11.jpg">
-                                  <img src="../static/images/home/11.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/11.jpg')}>
+                                <img src={require('../static/images/home/11.jpg')} width="500" height="333" alt="image" />
                                 </a>
 
-                                <a href="../static/images/home/12.jpg">
-                                  <img src="../static/images/home/12.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/12.jpg')}>
+                                <img src={require('../static/images/home/12.jpg')} width="500" height="333" alt="image" />
                                 </a>
 
-                                <a href="../static/images/home/13.jpg">
-                                  <img src="../static/images/home/13.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/13.jpg')}>
+                                <img src={require('../static/images/home/13.jpg')} width="500" height="333" alt="image" />
                                 </a>
 
-                                <a href="../static/images/home/14.jpg">
-                                  <img src="../static/images/home/14.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/14.jpg')}>
+                                <img src={require('../static/images/home/14.jpg')} width="500" height="333" alt="image" />
                                 </a>
                                 
-                                <a href="../static/images/home/15.jpg">
-                                  <img src="../static/images/home/15.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/15.jpg')}>
+                                <img src={require('../static/images/home/15.jpg')} width="500" height="333" alt="image" />
                                 </a>
 
-                                <a href="../static/images/home/16.jpg">
-                                <img src="../static/images/home/16.jpg" width="500" height="333" alt="image" />
+                                <a href={require('../static/images/home/16.jpg')}>
+                                <img src={require('../static/images/home/16.jpg')} width="500" height="333" alt="image" />
                                 </a>
                             </div>
 
